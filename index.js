3cade61cf938ea183ca053a6e38491319f1c37c8
Or Yaacov <oryaacov@live.com>
2023-05-16 02:31:13 +0300

    feat(devto-to-markdown): support react-markdown format

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const argv = require('minimist')(process.argv.slice(2));
+
 const url = argv.u;
+const reactMarkdown = argv["react-markdown"]
 
 const { convertPost } = require("./lib/devtoToMarkdown")
 
-convertPost(url)
+convertPost(url, reactMarkdown)
     .then(markdown => {
         console.log(markdown);
     }).catch(e => {
         console.error(e);
         process.exit(1);
     });
-
